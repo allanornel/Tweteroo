09b4c1c6fbefde19b8522d037e39c7bd9b5a5174
allanornel <allanornel@gmail.com>
2022-04-21 21:51:17 -0300

    feat: validação de dados e status error

--- index.js
+++ index.js
@@ -15,22 +15,37 @@ let tweets = [];
 
 app.post("/sign-up", (req, res) => {
   let user = req.body;
-  users.push(user);
-  console.log("OK");
-  res.send("OK");
+  if (!isEmpty(user) && user.username && user.avatar) {
+    users.push(user);
+    res.statusMessage = "Ok";
+    res.status(201).send();
+  } else {
+    res.statusMessage = "Todos os campos são obrigatórios!";
+    res.status(400).send();
+  }
 });
 
 app.post("/tweets", (req, res) => {
   let tweet = req.body;
-  let user = users.find((user) => user.username === tweet.username);
-  tweets.push({ ...tweet, avatar: user.avatar });
-  console.log("OK");
-  res.send("OK");
+  if (!isEmpty(tweet) && tweet.username && tweet.tweet) {
+    let user = users.find((user) => user.username === tweet.username);
+    tweets.push({ ...tweet, avatar: user.avatar });
+    res.statusMessage = "Ok";
+    res.status(201).send();
+  } else {
+    res.statusMessage = "Todos os campos são obrigatórios!";
+    res.status(400).send();
+  }
 });
 
 app.get("/tweets", (req, res) => {
   //retornar array com os últimos 10 tweets
+  res.status(200);
   res.send(tweets.slice(-10));
 });
 
 app.listen(5000);
+
+function isEmpty(obj) {
+  return Object.keys(obj).length === 0;
+}
