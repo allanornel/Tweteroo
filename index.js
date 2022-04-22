3438d0784f27bcded6281e484eee65b7714b0477
allanornel <allanornel@gmail.com>
2022-04-21 21:16:11 -0300

    feat: end of project back-end

--- index.js
+++ index.js
@@ -2,10 +2,35 @@ import express from "express";
 import cors from "cors";
 
 const app = express(); // cria um servidor
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+let users = [];
+let tweets = [];
+
+app.post("/sign-up", (req, res) => {
+  let user = req.body;
+  users.push(user);
+  console.log("OK");
+  res.send("OK");
+});
+
+app.post("/tweets", (req, res) => {
+  let tweet = req.body;
+  let user = users.find((user) => user.username === tweet.username);
+  tweets.push({ ...tweet, avatar: user.avatar });
+  console.log("OK");
+  res.send("OK");
+});
+
+app.get("/tweets", (req, res) => {
+  //retornar array com os últimos 10 tweets
+  res.send(tweets.slice(-10));
 });
 
 app.listen(5000);
